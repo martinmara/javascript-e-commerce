cb85c467ae8b562f5d5f15b7a6fe860d280e8f13
martin <70706555+martinmara@users.noreply.github.com>
2023-08-30 22:09:53 +0200

    commit number 4

---
+++ data-modules/products.js
@@ -0,0 +1,101 @@
+export const products = [
+  {
+    id: "1111-3333",
+    image: "images/download (6).jpg",
+    name: "Apple Air Tag",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceCents: "2344",
+  },
+  {
+    id: "1212-1111",
+    image: "images/download (7).jpg",
+    name: "Apple Airpods Pro 2",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceCents: "12033",
+  },
+  {
+    image: "images/61+Al0YynpL.jpg",
+    name: "Sony ZX Series ",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceCents: "19444",
+  },
+  {
+    id: "7675-1231",
+    image: "images/61YXnbbNrgL._AC_UL600_FMwebp_QL65_.webp",
+    name: "Fitbit Charge 5",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceCents: "1955",
+  },
+  {
+    id: "4345-1321",
+    image: "images/download (7).jpg",
+    name: "Apple Airpods Pro 2",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceCents: "12034",
+  },
+  {
+    id: "9865-2345",
+    image: "images/download (7).jpg",
+    name: "Apple Airpods Pro 2",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceCents: "12022",
+  },
+  {
+    id: "7574-2321",
+    image: "images/download (7).jpg",
+    name: "Apple Airpods Pro 2",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceCents: "1202",
+  },
+  {
+    id: "4356-3213",
+    image: "images/download (7).jpg",
+    name: "Apple Airpods Pro 2",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceCents: "2000",
+  },
+  {
+    id: "3343-1213",
+    image: "images/download (7).jpg",
+    name: "Apple Airpods Pro 2",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceCents: "1200",
+  },
+  {
+    id: "1234-3213",
+    image: "images/download (7).jpg",
+    name: "Apple Airpods Pro 2",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceCents: "12022",
+  },
+];
